e605ae28a8bb500180fa3ac350202256b8415112
dzx <dzxdetianxuan@outlook.com>
2025-04-22 21:23:50 +0800

    1.4封装css变量，给不同路由项做了不同颜色的贴合阴影高光

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,17 +8,17 @@ const Header= () =>  {
   const pathname = usePathname()
   // 跳转链接数据
   const LinkData = [
-    {name: 'corridor',link: '/performance'},
-    {name: 'village',link: '/reliability'},
-    {name: 'castle',link: '/scale'},
+    {name: 'corridor',link: '/performance',highLightColor: 'text-white text-shadow-close-white'},
+    {name: 'village',link: '/reliability',highLightColor: 'text-purple-600 text-shadow-close-purple'},
+    {name: 'castle',link: '/scale',highLightColor: 'text-black text-shadow-close-black'},
   ]
   return (
     <div className='absolute w-full z-10'>
-      <div className=' flex justify-between py-8  container mx-auto text-white'>
-          <Link href="/" className='text-3xl font-bold'>Home</Link>
+      <div className=' flex justify-between py-8  container mx-auto text-gray-300'>
+          <Link href="/" className={`${pathname == '/' ? 'text-white text-shadow-close-white' : ''} text-3xl font-bold`}>Home</Link>
           <div className='flex gap-5 text-xl'>
             {
-              LinkData.map((item) => <Link href={item.link} key = {item.name} className={`${pathname == item.link ? 'text-purple-500' : ''}`}>{item.name}</Link>)
+              LinkData.map((item) => <Link href={item.link} key = {item.name} className={`${pathname == item.link ? `${item.highLightColor} scale-110 ` : ''}`}>{item.name}</Link>)
             }
           </div>
       </div>
